f08358c416df516bdb2853af3fca0195d50fca8c
Gavin Staniforth <gavin@usemarkup.com>
2017-06-16 22:34:36 +0100

    chore: add metadata info

--- Gemfile
+++ Gemfile
@@ -2,10 +2,12 @@ source "https://rubygems.org"
 
 # Chef
 gem "berkshelf", "~> 6"
-gem "chef", ">= 12.6.0"
+gem "chef", "~> 12.7.2"
 gem 'knife-solo'
 
 gem "inspec"
 gem "test-kitchen"
 gem "kitchen-docker"
 gem 'kitchen-inspec'
+
+gem 'stove'
--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,11 @@ license 'MIT Licence'
 description 'phabricator project'
 version '0.1.0'
 
+maintainer 'Gavin Staniforth'
+maintainer_email 'gavin@usemarkup.com'
+
+source_url 'https://github.com/usemarkup/php-omnibus'
+
 supports 'centos', '>= 6.8'
 
 depends 'yum-epel'
