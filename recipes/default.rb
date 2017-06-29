c32843104cfbb1254ecacb5e634525cad3c45e0b
Gavin Staniforth <gavin@usemarkup.com>
2017-06-29 23:17:17 +0100

    fix: correctly set the opcache settings

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,7 @@
 node.default['php']['packages'] = [
   'php71u-cli',
   'php71u-intl',
+  'php71u-curl',
   'php71u-fpm',
   'php71u-mbstring',
   'php71u-json',
@@ -17,13 +18,6 @@ node.default['php']['directives'] = {
   'realpath_cache_ttl' => 7200,
 
   'error_log' => 'syslog',
-
-  'opcache.fast_shutdown' => 1,
-  'opcache.enable_cli' => 0,
-
-  'opcache.memory_consumption' => 128,
-  'opcache.interned_strings_buffer' => 64,
-  'opcache.max_accelerated_files' => 25000,
 }
 
 include_recipe 'php'
@@ -34,3 +28,19 @@ node.default['php-fpm']['package_name'] = 'php71u-fpm'
 node.default['php-fpm']['service_name'] = 'php-fpm'
 
 include_recipe 'php-fpm'
+
+node.default['php']['opcache-directives'] = {
+  'opcache.fast_shutdown' => 1,
+  'opcache.enable_cli' => 0,
+
+  'opcache.memory_consumption' => 128, # Assumes you have at least 128M of RAM to spare
+  'opcache.interned_strings_buffer' => 64,
+  'opcache.max_accelerated_files' => 25000,
+}
+
+# Opcache settings need to be stored saved in /etc/php.d/ due to system defaults in 10-opcache
+
+file '/etc/php.d/zz-opcache.ini' do
+  content node['php']['opcache-directives'].map { |k, v| "#{k}=#{v}" }.join("\n")
+  mode '0664'
+end
