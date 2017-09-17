c0a04174240953b6c991405862f5accc0b3a1256
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-09-17 12:13:13 +0100

    chore: release 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'php-omnibus'
 license 'MIT Licence'
 description 'an omnibus of packages to ease installing php'
-version '0.1.7'
+version '0.2.0'
 
 maintainer 'Gavin Staniforth'
 maintainer_email 'gavin@usemarkup.com'
