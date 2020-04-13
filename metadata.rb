5168bc971e8725f018670fae293da6c16413974a
Gavin Staniforth <gavin@usemarkup.com>
2020-04-13 15:15:19 +0100

    release: 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'php-omnibus'
 description 'an omnibus of packages to ease installing php'
-version '0.4.4'
+version '1.0.0'
 
 maintainer 'Gavin Staniforth'
 maintainer_email 'gavin@usemarkup.com'
